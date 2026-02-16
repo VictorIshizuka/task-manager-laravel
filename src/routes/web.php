e192f886bc24c8e2e7040a6526ecd9f5044a960c
Victor Rosario Ishizuka <127458647+VictorIshizuka@users.noreply.github.com>
2026-02-16 11:24:52 -0300

    fix: ajustes de rotas

--- src/routes/web.php
+++ src/routes/web.php
@@ -27,25 +27,25 @@ Route::middleware('auth')->group(function () {
     Route::resource('projects', ProjectController::class);
 
     // Project members routes
-    Route::post('projects/{project}/members', [ProjectController::class, 'addMember'])->name('projects.members.add');
-    Route::delete('projects/{project}/members/{user}', [ProjectController::class, 'removeMember'])->name('projects.members.remove');
+    Route::post('/projects/{project}/members', [ProjectController::class, 'addMember'])->name('projects.members.store');
+    Route::delete('/projects/{project}/members/{user}', [ProjectController::class, 'removeMember'])->name('projects.members.destroy');
 
     // Project files routes
-    Route::get('projects/{project}/files/{file}/download', [ProjectFileController::class, 'download'])->name('projects.files.download');
-    Route::post('projects/{project}/files', [ProjectFileController::class, 'store'])->name('projects.files.store');
-    Route::delete('projects/{project}/files/{file}', [ProjectFileController::class, 'destroy'])->name('projects.files.destroy');
+    Route::post('/projects/{project}/files', [ProjectFileController::class, 'store'])->name('projects.files.store');
+    Route::get('/projects/{project}/files/{file}/download', [ProjectFileController::class, 'download'])->name('projects.files.download');
+    Route::delete('/projects/{project}/files/{file}', [ProjectFileController::class, 'destroy'])->name('projects.files.destroy');
 
 
     // Task routes
     Route::resource('projects.tasks', TaskController::class);
 
     //  Task toggle status route
-    Route::patch('projects/{project}/tasks/{task}/toggle', [TaskController::class, 'toggle'])->name('projects.tasks.toggle');
+    Route::patch('/projects/{project}/tasks/{task}/toggle', [TaskController::class, 'toggle'])->name('projects.tasks.toggle');
 
     // Task files routes
-    Route::get('projects/{project}/tasks/{task}/files/{file}/download', [TaskFileController::class, 'download'])->name('projects.tasks.files.download');
-    Route::post('projects/{project}/tasks/{task}/files', [TaskFileController::class, 'store'])->name('projects.tasks.files.store');
-    Route::delete('projects/{project}/tasks/{task}/files/{file}', [TaskFileController::class, 'destroy'])->name('projects.tasks.files.destroy');
+    Route::get('/projects/{project}/tasks/{task}/files/{file}/download', [TaskFileController::class, 'download'])->name('projects.tasks.files.download');
+    Route::post('/projects/{project}/tasks/{task}/files', [TaskFileController::class, 'store'])->name('projects.tasks.files.store');
+    Route::delete('/projects/{project}/tasks/{task}/files/{file}', [TaskFileController::class, 'destroy'])->name('projects.tasks.files.destroy');
 });
 
 require __DIR__ . '/auth.php';
